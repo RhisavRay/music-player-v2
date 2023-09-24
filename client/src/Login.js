6b5c4da2edbe5d128ea3af2ac1e0c00df371a601
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-09-24 20:32:55 +0530

    AUTH_URL added

---
+++ client/src/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AUTH_URL = 'https://accounts.spotify.com/authorize?client_id=4cf947749ec84caea0b818ba1dc1e5f6&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state'
+
+const Login = () => {
+    return (
+        <div>Login</div>
+    )
+}
+
+export default Login
