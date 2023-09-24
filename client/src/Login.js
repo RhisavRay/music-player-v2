1d4a1ee8f5d315ae19b475d4a58ad629724db4be
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-09-24 21:12:29 +0530

    Login button styled

--- client/src/Login.js
+++ client/src/Login.js
@@ -6,7 +6,7 @@ const AUTH_URL = 'https://accounts.spotify.com/authorize?client_id=4cf947749ec84
 
 const Login = () => {
     return (
-        <Container>
+        <Container className='d-flex justify-content-center align-items-center' style={{ minHeight: "100vh" }}>
             <a className='btn btn-success btn-lg' href={AUTH_URL}>Login with Spotify</a>
         </Container>
     )
