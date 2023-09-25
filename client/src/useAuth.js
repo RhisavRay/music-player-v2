f914d81fe8c0d3ef0bffa4f93cd7c183ef1db26e
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-09-25 08:27:21 +0530

    Installed axios in server

--- client/src/useAuth.js
+++ client/src/useAuth.js
@@ -1,2 +1,15 @@
 // A custom hook to handle all the logic behind the login, authentication and refresh tokens
 
+import React, { useEffect, useState } from 'react'
+
+const useAuth = (code) => {
+  const [accessToken, setAccessToken] = useState();
+  const [refreshToken, setRefreshToken] = useState();
+  const [expiresIn, setExpiresIn] = useState();
+
+  useEffect(() => {
+    effect
+  }, [code])
+}
+
+export default useAuth
